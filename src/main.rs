21cee7437978bfdb6b29c6f1e6a0286d15c60c36
faiz <faizahmad030904@gmail.com>
2025-07-19 01:27:04 +0530

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use anyhow::{Context, Result};
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(||format! ("Could not read the file '{}'", args.path.display()))?;
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+    Ok(())
+}
